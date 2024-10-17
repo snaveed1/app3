23e7b2af968dda68c021ec4d5076eb4bed2fad0c
snaveed1 <159036395+snaveed1@users.noreply.github.com>
2024-10-17 16:09:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,36 @@ const port = 3003;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hope you are doing well from app3\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>App3</title>
+            <style>
+                body {
+                    background-color: lightyellow;
+                    margin: 0;
+                    height: 100vh;
+                    display: flex;
+                    justify-content: flex-end;
+                    align-items: start;
+                    font-size: 2em;
+                    font-weight: bold;
+                    text-align: right;
+                }
+                div {
+                    margin: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <div>Hope you are doing well from app3</div>
+        </body>
+        </html>
+    `);
 });
 
 server.listen(port, hostname, () => {
